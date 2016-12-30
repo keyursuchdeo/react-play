04b793437b785010acc0a6d0aad7c12812088ae1
Keyur Suchdeo <Keyur_Suchdeo@ad.infosys.com>
2016-12-30 17:46:09 +0530

    passing props from server side, added dynamic route

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,21 +2,24 @@ package controllers
 
 import play.api.mvc._
 import java.io.FileReader
+import javax.script.{ScriptContext, SimpleBindings}
+
+import play.api.libs.json.Json
 import utils.JSEngine
 
 object Application extends Controller {
 
-  def index = Action {
+  def index(name: String) = Action {
     val scriptEngine = JSEngine.init
     if (scriptEngine == null) {
       BadRequest("Nashorn script engine not found. Are you using JDK 8?")
     } else {
-      println("Loading js files")
       scriptEngine.eval(new FileReader("target/web/public/main/javascripts/components/Greetings.js"))
-      val initVal = Map (("name", "keyur"))
+      val prop =  "{'name':'" + name.toString +"'}";
       Ok(views.html.main("React on Play") {
-        play.twirl.api.Html(scriptEngine.eval("ReactDOMServer.renderToString(React.createElement(Greetings, {'name': 'keyur'}))").toString);
-      })
+        play.twirl.api.Html(
+          scriptEngine.eval("ReactDOMServer.renderToString(React.createElement(Greetings, {'name': 'keyur'}))").toString);
+      } (prop))
     }
   }
 }
